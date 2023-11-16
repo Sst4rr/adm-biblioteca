505398baf9ceb31c2decde82994f17af29b78f88
Thaís Ester Medeiros <thais.gomes@soulasalle.com.br>
2023-11-16 16:31:37 -0300

    classe livro pronta!

---
+++ Livro.java
@@ -0,0 +1,48 @@
+
+// Classe Livro como classe mãe
+class Livro {
+    private String titulo;
+    private String anoPublicacao;
+    private String autor;
+
+    public Livro(String titulo, String anoPublicacao, String autor) {
+        this.titulo = titulo;
+        this.anoPublicacao = anoPublicacao;
+        this.autor = autor;
+    }
+
+    // getter
+    
+    public String getTitulo(){
+        return this.titulo;
+    }
+
+    public String getAutor(){
+        return this.autor;
+    }
+
+    public String getAnoPublicacao(){
+        return this.anoPublicacao;
+    }
+    
+    //setters
+
+    public void setTitulo(String titulo){
+        this.titulo = titulo;
+    }
+
+    public void setAutor(String autor){
+        this.autor = autor;
+    }
+
+    public void setAnoPublicacao(String anoPublicacao){
+        this.anoPublicacao = anoPublicacao;
+    }
+    
+}
+
+
+
+
+
+
