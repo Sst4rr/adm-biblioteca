b627c80974b30e8da9d1495015c5e41167f8d052
vicentiin <vicentemirandaoffice@gmail.com>
2023-11-22 08:14:11 -0300

    Update LivroFisico.java

--- LivroFisico.java
+++ LivroFisico.java
@@ -3,13 +3,15 @@
  * Classe LivroFisico que representa um livro físico.
  */
 class LivroFisico extends Livro {
-    
+
     private int copiasDisponiveis;
+
     /**
      * Construtor da classe LivroFisico.
-     * @param titulo Titulo do livro
-     * @param anoPublicacao Ano de publicação do livro
-     * @param autor Autor do livro
+     * 
+     * @param titulo            Titulo do livro
+     * @param anoPublicacao     Ano de publicação do livro
+     * @param autor             Autor do livro
      * @param copiasDisponiveis Quantidade de cópias disponíveis do livro
      */
     public LivroFisico(String titulo, String anoPublicacao, String autor, int copiasDisponiveis) {
@@ -17,34 +19,35 @@ class LivroFisico extends Livro {
         this.copiasDisponiveis = copiasDisponiveis;
     }
 
+    // teste
 
-    public int getCopiasDisponiveis(){
+    public int getCopiasDisponiveis() {
         return this.copiasDisponiveis;
     }
-    
-    public void setCopiasDisponiveis(int copiasDisponiveis){
+
+    public void setCopiasDisponiveis(int copiasDisponiveis) {
         this.copiasDisponiveis = copiasDisponiveis;
     }
-    
-    
 
     /**
      * Método para emprestar um livro.
+     * 
      * @param usuario O usuário que está emprestando o livro
      */
-    public void emprestarlivro(Usuario usuario){
-        if(this.copiasDisponiveis > 0){
+    public void emprestarlivro(Usuario usuario) {
+        if (this.copiasDisponiveis > 0) {
             this.copiasDisponiveis--;
-        }else{
+        } else {
             System.out.println("Não há mais exemplares disponíveis. Tente de novo futuramente!");
         }
     }
 
     /**
      * Método para devolver um livro.
-     * @param usuario  O usuário que está devolvendo o livro.
+     * 
+     * @param usuario O usuário que está devolvendo o livro.
      */
-    public void devolverlivro(Usuario usuario){
+    public void devolverlivro(Usuario usuario) {
         this.copiasDisponiveis++;
     }
 }
